73f54425cc4e126d2e4d0c2ed57ca07890ea4f30
tehsven <tehsven@gmail.com>
2014-05-30 09:23:14 -0500

    Update rest_client.rb

--- ruby_scripts/rest_client.rb
+++ ruby_scripts/rest_client.rb
@@ -7,7 +7,7 @@ puts "example rest client"
 c = HTTPClient.new
 
 # set the credentials
-c.set_auth("http://test.shareplan.com:4280", "coder42@code42.com", "asdfasdf")
+c.set_auth("http://<url goes here>:4280", "coder42@code42.com", "<password goes here>")
 
 # safe to define the content type
 headers = {
@@ -15,11 +15,11 @@ headers = {
 }
 
 # make a get request against the "Plan" resource
-response = c.get("http://test.shareplan.com:4280/api/Plan")
+response = c.get("http://<url goes here>:4280/api/Plan")
 
 # this is how you would create a plan
 # planinfo = { 'name' => 'stuff', 'orgId' => 2 }
-#response = c.post_content("http://test.shareplan.com:4280/api/Plan", planinfo.to_json, headers)
+#response = c.post_content("http://<url goes here>:4280/api/Plan", planinfo.to_json, headers)
 
 # print out the response body
 puts response
